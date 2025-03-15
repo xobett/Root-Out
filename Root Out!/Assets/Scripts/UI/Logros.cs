8b641a5c3873dc67443cc5b1fd8a73e0127974d0
Alejandro <llaltairll@outlook.com>
2025-03-15 02:08:06 -0600

    LOgro por tomar arma

--- Root Out!/Assets/Scripts/UI/Logros.cs
+++ Root Out!/Assets/Scripts/UI/Logros.cs
@@ -1,16 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class Logros : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private WeaponHandler weaponHandler;
+    [SerializeField] Animation primeraArma;
+    [SerializeField] Animation logoXP;
+
+
+    private void Start()
     {
-        
+        primeraArma.Stop();
+        logoXP.Stop();
+    }
+    private void Update()
+    {
+       StartCoroutine(PrimeraArma());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator PrimeraArma()
     {
-        
+        if (weaponHandler != null && weaponHandler.currentWeapon != null)
+        {
+            primeraArma.Play();
+            logoXP.Play();
+            yield return new WaitForSeconds(4f);
+            primeraArma.Stop(); 
+            logoXP.Stop();
+        }
     }
 }
