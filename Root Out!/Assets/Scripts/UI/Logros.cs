b9f45953b5028e00f2893a26c8a22dabc7003d3d
Alejandro <llaltairll@outlook.com>
2025-03-15 21:36:47 -0600

    Avance

--- Root Out!/Assets/Scripts/UI/Logros.cs
+++ Root Out!/Assets/Scripts/UI/Logros.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Logros : MonoBehaviour
@@ -9,9 +10,9 @@ public class Logros : MonoBehaviour
     [SerializeField] Animation animacionPrimeraArma;
 
     [Header("Enemigos Derrotados")]
-    [SerializeField] int enemiesDefeat;
     [SerializeField] Animation animacion10Enemigos;
     [SerializeField] GameObject canvas10Enemigos;
+    private int enemiesDefeat;
 
     private void Start()
     {
@@ -31,6 +32,7 @@ public class Logros : MonoBehaviour
     {
         if (scriptWeaponHandler.currentWeapon != null)
         {
+            AudioManager.instance.PlaySFX("Logros");
             animacionPrimeraArma.Play();
             logroCanvas.SetActive(true);
             yield return new WaitForSeconds(4f);
@@ -42,15 +44,22 @@ public class Logros : MonoBehaviour
 
     public IEnumerator NumberEnemmiesDefeat()
     {
-        enemiesDefeat = AIHealth.enemiesDefeated; // Obtiene el contador de enemigos derrotados
-        if (enemiesDefeat == 10)
+        while (true)
         {
-            animacion10Enemigos.Play();
-            canvas10Enemigos.SetActive(true);
-            yield return new WaitForSeconds(4f);
-            animacion10Enemigos.Stop();
-            canvas10Enemigos.SetActive(false);
-            StopCoroutine(NumberEnemmiesDefeat());
+            enemiesDefeat = AIHealth.enemiesDefeated; // Obtener el contador de enemigos derrotado
+
+            if (enemiesDefeat == 10)
+            {
+                AudioManager.instance.PlaySFX("Logros");
+                animacion10Enemigos.Play();
+                canvas10Enemigos.SetActive(true);
+                yield return new WaitForSeconds(4f);
+                animacion10Enemigos.Stop();
+                canvas10Enemigos.SetActive(false);
+                StopCoroutine(NumberEnemmiesDefeat());
+            }
+
+            yield return new WaitForSeconds(1f); // Actualizar cada segundo
         }
     }
 }
